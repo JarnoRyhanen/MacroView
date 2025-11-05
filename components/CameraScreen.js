64186a0ade15ee3c90bd8d948fe6b970cfea6c00
JarnoRyhanen <jarno.ryhanen02@gmail.com>
2025-11-05 16:47:22 +0200

    Remove expo camera functionality and update dependencies

--- components/CameraScreen.js
+++ components/CameraScreen.js
@@ -1,58 +1,19 @@
-import { StyleSheet, Text, View, Button, Image } from 'react-native';
-import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
-import { useRef, useState } from 'react';
-
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function CameraScreen() {
-    const [photoName, setPhotoName] = useState('');
-    const [photoBase64, setPhotoBase64] = useState('');
-    const [permission, requestPermission] = useCameraPermissions();
-    const camera = useRef(null);
-
-
-    if (!permission) {
-        return <View />;
-    }
-
-    if (!permission.granted) {
-        return (
-            <View style={styles.container}>
-                <Button onPress={requestPermission} title="grant permission" />
-            </View>
-        );
-    }
-    const snap = async () => {
-        if (camera) {
-            const photo = await camera.current.takePictureAsync({ base64: true });
-            setPhotoName(photo.uri);
-            setPhotoBase64(photo.base64);
-        }
-    };
-
-    return (
-        <View style={{ flex: 1 }}>
-            <CameraView style={{ flex: 1, minWidth: "100%" }} ref={camera} />
-            <Button title="Take Photo" onPress={snap} />
-            <View style={{ flex: 1 }}>
-                {photoName && photoBase64 ? (
-                    <>
-                        <Image style={{ flex: 1 }} source={{ uri: photoName }} />
-                        {/*    <Image style={{ flex: 1 }} source={{ uri: `data:image/jpg;base64,${photoBase64}` }} /> */}
-                    </>
-                ) : (
-                    <Text>No photo taken yet.</Text>
-                )}
-            </View>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text>Camera removed from this project.</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
 
