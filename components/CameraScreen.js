4558a42d7e9fbac0346186cc0af1db6fca0bb845
JarnoRyhanen <jarno.ryhanen02@gmail.com>
2025-11-06 19:05:15 +0200

    Add MobileNet model and integrate object detection in CameraScreen
    - Added mobilenet.tflite model to assets for object detection.
    - Updated CameraScreen.js to utilize TensorFlow model for detecting objects in camera frames.
    - Integrated vision-camera-resize-plugin to resize frames before processing.
    - Updated package.json and package-lock.json to include vision-camera-resize-plugin dependency.
    -Added mobilenetV2 dataset for image recognition

--- components/CameraScreen.js
+++ components/CameraScreen.js
@@ -2,8 +2,10 @@ import { useAppState } from '@react-native-community/hooks';
 import { useIsFocused } from '@react-navigation/native';
 import { useEffect, useRef, useState } from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
+import { loadTensorflowModel, useTensorflowModel } from 'react-native-fast-tflite';
 import { Camera, useCameraDevice, useCameraPermission, useFrameProcessor } from 'react-native-vision-camera';
 import { useImageLabeler } from 'react-native-vision-camera-v3-image-labeling';
+import { useResizePlugin } from 'vision-camera-resize-plugin';
 
 export default function CameraScreen() {
 
@@ -13,16 +15,37 @@ export default function CameraScreen() {
   const isFocused = useIsFocused();
   const appState = useAppState();
   const isActive = isFocused && appState === "active"
+  const { resize } = useResizePlugin();
 
   const options = { minConfidence: 0.8 };
   const { scanImage } = useImageLabeler(options);
 
-  const frameProcessor = useFrameProcessor((frame) => {
-    'worklet'
-    const data = scanImage(frame);
-    console.log(data[0]);
-  }, []);
+  const objectDetection = useTensorflowModel(require('../assets/mobilenet.tflite'))
+  const model =
+    objectDetection.state === 'loaded' ? objectDetection.model : undefined
 
+
+  const frameProcessor = useFrameProcessor(
+    (frame) => {
+      'worklet'
+      if (model == null) return
+
+      const resized = resize(frame, {
+        scale: {
+          width: 244,
+          height: 244,
+        },
+        pixelFormat: 'rgb',
+        dataType: 'uint8',
+      })
+
+      const outputs = model.runSync([resized])
+
+      const num_detections = outputs[0]
+      console.log(`Detected ${num_detections} objects!`)
+    },
+    [model]
+  )
   useEffect(() => {
     if (!hasPermission) {
       requestPermission();
