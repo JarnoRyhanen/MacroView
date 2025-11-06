87f20014fed5e2cf46510d689df592496687b2a5
JarnoRyhanen <jarno.ryhanen02@gmail.com>
2025-11-06 11:33:46 +0200

    Refactor CameraScreen to include permission handling and improve layout

--- components/CameraScreen.js
+++ components/CameraScreen.js
@@ -1,4 +1,5 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 import { Camera, useCameraDevice, useCameraPermission } from 'react-native-vision-camera';
 
 export default function CameraScreen() {
@@ -6,12 +7,38 @@ export default function CameraScreen() {
   const { hasPermission, requestPermission } = useCameraPermission()
   const device = useCameraDevice('back')
 
+  useEffect(() => {
+    if (!hasPermission) {
+      requestPermission();
+    }
+  }, [hasPermission, requestPermission]);
+
+  if (!hasPermission) {
+    return <Text>Grant camera access to use the camera</Text>
+  }
+
+  const scan = () => {
+    console.log("Scanned");
+  }
+
   return (
-    <Camera
-      style={StyleSheet.absoluteFill}
-      device={device}
-      isActive={true}
-    />
+    <View style={styles.container}>
+      {device && (
+        <Camera
+          style={StyleSheet.absoluteFill}
+          device={device}
+          isActive={true}
+        />
+      )}
+
+      <View style={styles.bottom}>
+        <View style={styles.buttonWrapper}>
+          <Button
+            title='Scan'
+            onPress={() => scan()} />
+        </View>
+      </View>
+    </View>
   );
 }
 
@@ -19,8 +46,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  bottom: {
+    position: 'absolute',
+    left: 20,
+    right: 20,
+    bottom: 30,
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  buttonWrapper: {
+    width: '100%',
   },
 });
 
