5d379122819603b90eab5e5909a1cbe3185dafa8
JarnoRyhanen <jarno.ryhanen02@gmail.com>
2025-11-07 19:41:14 +0200

    Enhance Babel configuration and add utility functions for label loading and prediction
    - Updated Babel configuration to include `processNestedWorklets` in the react-native-reanimated plugin.
    - Added a new utility module (utils.js) that includes:
      - `loadLabels`: Function to load and validate labels from imagenet_labels.json.
      - `getBestPrediction`: Worklet function to determine the best prediction from an output array based on confidence levels.

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,7 @@ module.exports = {
   presets: ['babel-preset-expo'],
   plugins: [
     // react-native-reanimated plugin must appear once and last
-    ['react-native-reanimated/plugin', { globals: ['__labelImage'] }],
+    ['react-native-reanimated/plugin', { globals: ['__labelImage'], processNestedWorklets: true }],
     ['react-native-worklets-core/plugin']
   ],
 };
---
+++ utils.js
@@ -0,0 +1,43 @@
+
+import labels from "./assets/imagenet_labels.json";
+export const loadLabels = () => {
+    try {
+        if (!Array.isArray(labels)) {
+            console.error("Labels file is not a valid JSON array");
+            return [];
+        }
+        console.log(`Loaded labels from imagenet_labels.json`);
+
+        return labels;
+    } catch (error) {
+        console.error("Failed to load imagenet_labels.json:", error);
+        return [];
+    }
+};
+
+export const getBestPrediction = (outputArray, labels) => {
+    'worklet'
+    if (!outputArray || outputArray.length === 0) return "No prediction";
+
+    const probabilities = outputArray[0];
+    if (!probabilities || probabilities.length === 0) return "No prediction";
+
+    let maxProbability = -Infinity;
+    let predictedIndex = -1;
+
+    for (let i = 0; i < probabilities.length; i++) {
+        if (probabilities[i] > maxProbability) {
+            maxProbability = probabilities[i];
+            predictedIndex = i;
+        }
+    }
+
+    if (predictedIndex !== -1 && maxProbability > 0.5) {
+        const label = (labels && labels[predictedIndex]) ? labels[predictedIndex] : `Unknown Class (Index: ${predictedIndex})`;
+        const confidence = (maxProbability * 100).toFixed(2);
+/*         console.log(`${label} (${confidence}%`);
+ */        return `${label} (${confidence}%)`;
+    }
+
+    return "No object detected (Confidence too low)";
+};
